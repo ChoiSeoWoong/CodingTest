434cb74a491146ccc904e6492bd38b213494bb13
ChoiSeoWoong <chldbfkr456@naver.com>
2023-02-14 02:42:29 +0900

    230214 Commit

---
+++ src/main/java/문자열/프로_숫자문자열과영단어.java
@@ -0,0 +1,19 @@
+package 문자열;
+
+import java.util.*;
+
+public class 프로_숫자문자열과영단어 {
+    class Solution {
+        public int solution(String s) {
+            int answer = 0;
+            String[] engNum = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+            for(int i = 0; i < engNum.length; i++) {
+                s = s.replace(engNum[i], String.valueOf(i));
+            }
+
+            answer = Integer.parseInt(s);
+            return answer;
+        }
+    }
+}
