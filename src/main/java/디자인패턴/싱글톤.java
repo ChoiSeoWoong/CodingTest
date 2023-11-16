b81dcb0441001e55c0d73216b854b6e450dc8c82
user <chldbfkr456@naver.com>
2023-11-16 19:44:25 +0900

    2023-11-16 Commit - 싱글톤 구현 및 Enum 구현

---
+++ src/main/java/디자인패턴/싱글톤.java
@@ -0,0 +1,22 @@
+package 디자인패턴;
+
+class Singleton {
+    private static class singleInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return singleInstanceHolder.INSTANCE;
+    }
+}
+
+public class 싱글톤 {
+    public static void main(String[] args) {
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+        System.out.println(a == b);
+    }
+}
