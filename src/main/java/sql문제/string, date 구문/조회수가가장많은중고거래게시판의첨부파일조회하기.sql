77650d2d1834f37450bff46e283015edbdc4aeb8
user <chldbfkr456@naver.com>
2024-03-15 19:13:39 +0900

    2024-03-15 Commit - SQL 풀이

---
+++ src/main/java/sql문제/string, date 구문/조회수가가장많은중고거래게시판의첨부파일조회하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT concat('/home/grep/src/', f.board_id, '/', f.file_id, f.file_name, f.file_ext)
+as file_path from used_goods_board as b join used_goods_file as f on b.board_id = f.board_id
+where views in(select max(views) from used_goods_board)
+order by f.file_id desc
