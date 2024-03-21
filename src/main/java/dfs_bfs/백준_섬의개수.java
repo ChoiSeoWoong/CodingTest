9ba402d1a05199068922cacf7692a5f6b55d9922
user <chldbfkr456@naver.com>
2024-03-21 18:37:28 +0900

    2024-03-21 Commit - 알고리즘 풀이

--- src/main/java/dfs_bfs/백준_섬의개수.java
+++ src/main/java/dfs_bfs/백준_섬의개수.java
@@ -1,9 +1,88 @@
 package dfs_bfs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class 백준_섬의개수 {
-    public static void main(String[] args) {
+    static int[][] graph;
+    static boolean[][] visited;
+    static int h, w;
 
+    static class Node {
+        int x, y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private static void bfs(int a, int b) {
+        int[] dx = {0, 0, -1, 1, -1, 1, -1, 1};
+        int[] dy = {-1, 1, 0, 0, 1, 1, -1, -1};
+
+        Queue<Node> queue = new LinkedList<>();
+        visited[a][b] = true;
+        queue.add(new Node(a, b));
+
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            int x = node.x;
+            int y = node.y;
+
+            for(int i = 0; i < 8; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx < 0 || ny < 0 || nx >= h || ny >= w) {
+                    continue;
+                }
+
+                if(!visited[nx][ny] && graph[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    queue.add(new Node(nx, ny));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+            if(w == 0 && h == 0) {
+                break;
+            }
+
+            graph = new int[h][w];
+            visited = new boolean[h][w];
+
+            for(int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                for(int j = 0; j < w; j++) {
+                    graph[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int cnt = 0;
+
+            for(int i = 0; i < h; i++) {
+                for(int j = 0; j < w; j++) {
+                    if(!visited[i][j] && graph[i][j] == 1) {
+                        bfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
+
+            System.out.println(cnt);
+        }
     }
 }
