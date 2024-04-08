5d3407f65ad9d85456fac29c22d2794811149ad2
user <chldbfkr456@naver.com>
2024-04-08 18:49:46 +0900

    2024-04-08 Commit - SQL 풀이

---
+++ src/main/java/sql문제/null 처리 구문/업그레이드할수없는아이템구하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 작성해주세요
+select i.item_id, i.item_name, i.rarity from item_info as i
+join item_tree as t on i.item_id = t.item_id
+where i.item_id not in(select parent_item_id from item_tree
+                       where parent_item_id is not null)
+order by 1 desc
