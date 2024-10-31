5a1eec5012f05dd8da3a715e0342d6505fb5b19e
CHOISEOWOONG <chldbfkr456@naver.com>
2024-10-31 20:40:55 +0900

    2024-10-31 Commit - 알고리즘 풀이

---
+++ src/main/java/구현/프로그래머스_할인행사.java
@@ -0,0 +1,37 @@
+package 구현;
+
+import java.util.*;
+
+public class 프로그래머스_할인행사 {
+    class Solution {
+        public int solution(String[] want, int[] number, String[] discount) {
+            int answer = 0;
+            Map<String, Integer> quantity = new HashMap<>();
+
+            for(int i = 0; i < number.length; i++) {
+                quantity.put(want[i], number[i]);
+            }
+
+            for(int i = 0; i < discount.length - 9; i++) {
+                Map<String, Integer> countMap = new HashMap<>();
+
+                for(int j = 0; j < 10; j++) {
+                    countMap.put(discount[i + j], countMap.getOrDefault(discount[i + j], 0) + 1);
+                }
+
+                boolean flag = true;
+
+                for(String key : quantity.keySet()) {
+                    if(quantity.get(key) != countMap.get(key)) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                answer += flag ? 1 : 0;
+            }
+
+            return answer;
+        }
+    }
+}
