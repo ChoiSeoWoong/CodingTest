f1990ad875ccbd8b6753009ad96783f42b43558e
CHOISEOWOONG <chldbfkr456@naver.com>
2024-12-09 23:33:33 +0900

    2024-12-09 Commit - 알고리즘 풀이

---
+++ src/main/java/dfs_bfs/프로그래머스_네트워크.java
@@ -0,0 +1,38 @@
+package dfs_bfs;
+
+import java.util.*;
+
+public class 프로그래머스_네트워크 {
+    class Solution {
+        public static void bfs(int i, int[][] computers, boolean[] visited) {
+            Queue<Integer> queue = new LinkedList<>();
+
+            queue.add(i);
+            visited[i] = true;
+
+            while(!queue.isEmpty()) {
+                int popIndex = queue.poll();
+                for(int j = 0; j < computers.length; j++) {
+                    if(popIndex != j && visited[j] == false && computers[popIndex][j] == 1) {
+                        visited[j] = true;
+                        queue.add(j);
+                    }
+                }
+            }
+        }
+
+        public int solution(int n, int[][] computers) {
+            int answer = 0;
+            boolean[] visited = new boolean[n];
+
+            for(int i = 0; i < computers.length; i++) {
+                if(visited[i] == false) {
+                    bfs(i, computers, visited);
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
